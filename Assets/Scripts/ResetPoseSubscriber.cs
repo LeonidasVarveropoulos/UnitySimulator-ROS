a95406e84644eded0bffcf7fd9c0bd9564c0be43
LeonidasVarveropoulos <55664403+LeonidasVarveropoulos@users.noreply.github.com>
2020-05-28 00:08:36 -0500

    Uncovered problem with global and local pose

---
+++ Assets/Scripts/ResetPoseSubscriber.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class ResetPoseSubscriber : UnitySubscriber<MessageTypes.Std.Float32MultiArray>
+    {
+        public Transform SubscribedTransform;
+        private float[] messageData;
+        private bool gotData = false;
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        void Update()
+        {
+            if (gotData)
+            {
+                Debug.Log(SubscribedTransform.localPosition);
+                SubscribedTransform.localPosition = new Vector3((float)-messageData[1], (float)SubscribedTransform.localPosition.y, (float)messageData[0]);
+                SubscribedTransform.localEulerAngles = new Vector3(0, messageData[2] * 180f/Mathf.PI, 0);
+                gotData = false;
+            }
+        }
+
+        protected override void ReceiveMessage(MessageTypes.Std.Float32MultiArray message)
+        {
+            messageData = message.data;
+            gotData = true;
+        }
+    }
+}
