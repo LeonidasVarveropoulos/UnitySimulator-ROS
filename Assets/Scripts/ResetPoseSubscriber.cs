a1fd55ff4c0517c4b430487846bcc5bada27d7af
LeonidasVarveropoulos <55664403+LeonidasVarveropoulos@users.noreply.github.com>
2020-05-31 03:01:14 -0500

    Added global pose reset

--- Assets/Scripts/ResetPoseSubscriber.cs
+++ Assets/Scripts/ResetPoseSubscriber.cs
@@ -4,7 +4,8 @@ namespace RosSharp.RosBridgeClient
 {
     public class ResetPoseSubscriber : UnitySubscriber<MessageTypes.Std.Float32MultiArray>
     {
-        public Transform SubscribedTransform;
+        public Transform GlobalTransform;
+        public Transform LocalTransform;
         private float[] messageData;
         private bool gotData = false;
 
@@ -17,9 +18,11 @@ namespace RosSharp.RosBridgeClient
         {
             if (gotData)
             {
-                Debug.Log(SubscribedTransform.localPosition);
-                SubscribedTransform.localPosition = new Vector3((float)-messageData[1], (float)SubscribedTransform.localPosition.y, (float)messageData[0]);
-                SubscribedTransform.localEulerAngles = new Vector3(0, messageData[2] * 180f/Mathf.PI, 0);
+                GlobalTransform.localPosition = new Vector3((float)-messageData[1], (float)GlobalTransform.localPosition.y, (float)messageData[0]);
+                GlobalTransform.localEulerAngles = new Vector3(0, messageData[2] * 180f/Mathf.PI, 0);
+
+                LocalTransform.localPosition = new Vector3(0.0f, (float)LocalTransform.localPosition.y, 0.0f);
+                LocalTransform.localEulerAngles = new Vector3(0f, 0f, 0f);
                 gotData = false;
             }
         }
